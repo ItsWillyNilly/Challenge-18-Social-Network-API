de34267ffed77fd3c7ffd824d4c2355546d63f86
William Lee <williamlee@Williams-MacBook-Pro.local>
2024-08-19 09:50:07 -0700

    added routes, completed the server.js, and fixed some minor bugs

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const URIString = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
+const URIString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB';
 
 connect(URIString);
 
